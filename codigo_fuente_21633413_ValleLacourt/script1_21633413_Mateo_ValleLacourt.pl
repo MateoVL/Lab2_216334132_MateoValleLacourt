e2c5fef1fda01574878beb05e41641379dbce19d
Mateo Valle <mateo.valle@usach.cl>
2024-11-25 23:45:53 -0300

    Scripts Actualizados 1.0

--- codigo_fuente_21633413_ValleLacourt/script1_21633413_Mateo_ValleLacourt.pl
+++ codigo_fuente_21633413_ValleLacourt/script1_21633413_Mateo_ValleLacourt.pl
@@ -1,9 +1,3 @@
-:- use_module("main_21633413_ValleLacourt").
-:- use_module("TDAplayer_21633413_ValleLacourt").
-:- use_module("TDAboard_21633413_ValleLacourt").
-:- use_module("TDAgame_21633413_ValleLacourt").
-
-
 % 1. Crear jugadores (10 fichas cada uno para un juego corto)
 player(1, "Pedro", "rojo", 0, 0, 0, 21, P1),
 player(2, "Maria", "verde", 0, 0, 0, 21, P2),
--- codigo_fuente_21633413_ValleLacourt/script_base_21633413_Mateo_ValleLacourt.pl
+++ codigo_fuente_21633413_ValleLacourt/script_base_21633413_Mateo_ValleLacourt.pl
@@ -1,8 +1,3 @@
-:- use_module("main_21633413_ValleLacourt").
-:- use_module("TDAplayer_21633413_ValleLacourt").
-:- use_module("TDAboard_21633413_ValleLacourt").
-:- use_module("TDAgame_21633413_ValleLacourt").
-
 % 1. Crear jugadores (10 fichas cada uno para un juego corto)
 player(1, "Juan", "red", 0, 0, 0, 10, P1),
 player(2, "Mauricio", "yellow", 0, 0, 0, 10, P2),
